47b6836eec6bc44e105ed9237703ef8ab5857434
Alexis Sánchez <aasanchez@gmail.com>
2013-04-07 16:33:46 -0430

    Pinguino x.2 Files

---
+++ examples/4.Communication/USB/CDC/Printf.pde
@@ -0,0 +1,11 @@
+u8 n=0;
+
+void setup()
+{
+}
+
+void loop()
+{
+	CDC.printf("n = %d \r\n", n++);
+	delay(100);
+}
---
+++ source/define.h
@@ -0,0 +1,8 @@
+
+
+
+#define DEBUG_MODE NODEBUG
+#define PIC18F2550
+#include <analog.c>
+#include <common_types.h>
+#include <serial.c>
